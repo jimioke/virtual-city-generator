e493c19ae973fcf4b65355ec70fb59cdcea0c9fa
Jasmine1004 <zhoujin1004@gmail.com>
2017-08-08 12:37:39 -0400

    add marginal constraint and nd test bn

--- population/microdata_eda.R
+++ population/microdata_eda.R
@@ -1,6 +1,7 @@
 
 
-
+library(ipfp)
+library(plyr)
 # individual variables
 
 # household number: pcode
@@ -31,7 +32,8 @@ household <- read.table('data/21741-0002-Data.tsv', header=T, sep='\t')
 
 households_tb_kept <- individual[individual$p104 == 1, 'pcode']
 household <- subset(household, pcode %in%  households_tb_kept)
-individual <- individual[individual$p104==1,]
+individual <- individual[individual$p104==1 & !is.na(individual$pcode),]
+
 
 dim(individual)
 colnames(individual)
@@ -39,28 +41,45 @@ count(individual, 'city')
 plot(count(individual,'p106'),cex=0.5)
 
 
-inRawData <- individual[,c('pcode','p102','p105','p106','p107','p109','p112','p201')]
-hhRawData <- household[,c('pcode','h401','h501')]
+inRawData <- as.data.frame(individual[,c('pcode','p102','p103','p105','p106','p107','p109','p112','p201')])
+#inRawData <- individual[,c('pcode','p105','p106','p107','p109','p112','p201')]
+hhRawData <- as.data.frame(household[,c('pcode','h401','h501')])
 
 
-colnames(inRawData) <- c('pcode','num_in_hh','sex','age','emp_status','marriage','edu','inc')
+colnames(inRawData) <- c('pcode','hh_size','rel','sex','age','emp_status','marriage','edu','inc')
 colnames(hhRawData) <- c('pcode','fin_asset','loc_res')
+
+# hh_size:
+# categories: 1,2,3,4, >5
+for (i in unique(inRawData$pcode)){
+  inRawData[inRawData$pcode == i, 'hh_size'] <- max(inRawData[inRawData$pcode == i,'hh_size'])
+}
+inRawData$hh_size <- ifelse(inRawData$hh_size >= 5, 5, inRawData$hh_size)
+
+# rel: relationship with the head of the household
+# 1: self; 2:spous; 3:child; 4: grandchild; 5: parent; 6: other;
+rel_raw <- list(c(1),c(2),c(3,4),c(5), c(6,7), c(8,9,10,11))
+rel_ind <- list()
+for (i in 1:length(rel_raw)){
+  rel_ind <- c(rel_ind, list(which(inRawData$rel %in% rel_raw[[i]])))
+}
+for (i in 1:length(rel_raw)){
+  inRawData[rel_ind[[i]],'rel'] <- i
+}
+
 
 # sex: male=1, female=2
 # age: 0-9 = 1, 10-19 = 2, 20-29 = 3, ... 50-59 = 6, >=60 = 7
 # work: 
 #   1. working or imployment: 1; 
 #   2. student: 12
-#   3. unable to work: 4
-#   4. retired: 3,7,8
-#   5. unemployed: 2,5,6,9,
-#   6. full-time homemaker: 11
-#   7. wait for job assignment: 10,13
-#   8. others: 14, 0
-#   9. missing
+#   3. retired: 3,7,8
+#   4. unemployed: 2,4,5,6,9,10,11,13
+#   5. others: 14, 0, missing
+
 
 inRawData$age <- ifelse(inRawData$age>=60, 7, as.integer(inRawData$age/10+1))
-emp_raw <- list(c(1),c(12),c(4),c(3,7,8), c(2,5,6,9), c(11), c(10,13), c(14,0))
+emp_raw <- list(c(1),c(12),c(3,7,8), c(2,4,5,6,9,10,11,13), c(14,0,NA))
 emp_ind <- list()
 for (i in 1:length(emp_raw)){
   emp_ind <- c(emp_ind, list(which(inRawData$emp_status %in% emp_raw[[i]])))
@@ -70,10 +89,9 @@ for (i in 1:length(emp_raw)){
 }
 
 # Marriage: 
-# Never married: 1, with spous: 2, divorced: 3
-# widow or widower: 4, other: 0,5  = 5, missing: NA
+# Not married: 1; Married: 2;
 
-inRawData$marriage <- ifelse(inRawData$marriage == 0, 5, inRawData$marriage)
+inRawData$marriage <- ifelse(inRawData$marriage == 2, 2, 1)
 
 
 # Education:
@@ -84,32 +102,32 @@ inRawData$marriage <- ifelse(inRawData$marriage == 0, 5, inRawData$marriage)
 # 5. University/graduate: 8,9
 # 6. Other: 0
 
-mrg_raw <- list(c(1),c(2,3),c(4,5),c(6,7),c(8,9),c(0))
-mrg_ind <- list()
-for (i in 1:length(mrg_raw)){
-  mrg_ind <- c(mrg_ind, list(which(inRawData$marriage %in% mrg_raw[[i]])))
+edu_raw <- list(c(1),c(2,3),c(4,5),c(6,7),c(8,9),c(0,NA))
+edu_ind <- list()
+for (i in 1:length(edu_raw)){
+  edu_ind <- c(edu_ind, list(which(inRawData$edu %in% edu_raw[[i]])))
 }
-for (i in 1:length(mrg_raw)){
-  inRawData[mrg_ind[[i]],'marriage'] <- i
+for (i in 1:length(edu_raw)){
+  inRawData[edu_ind[[i]],'edu'] <- i
 }
 
 # income
-# 
-inc_range <- array(NA,c(7,2))
-inc_range[1,] <- c(0, 5000)
-inc_range[2,] <- c(5000, 10000)
-inc_range[3,] <- c(10000, 15000)
-inc_range[4,] <- c(15000, 20000)
-inc_range[5,] <- c(20000, 30000)
-inc_range[6,] <- c(30000, 50000)
-inc_range[7,] <- c(50000, max(inRawData$inc,na.rm = T)+1)
+# 6 categories
+inRawData$inc <- cut(inRawData$inc, breaks=c(0,5000,10000,15000,20000,30000,max(inRawData$inc,na.rm = T)),labels = 1:6,include.lowest = T)
 
-for (i in 1:dim(inc_range)[1]){
-  inRawData$inc <- replace(inRawData$inc, inRawData$inc >= inc_range[i,1] & inRawData$inc < inc_range[i,2],i)
-}
+#inContTable <- table(inRawData[,2:dim(inRawData)[2]])
+inContTable <- xtabs(~hh_size+rel+sex+age+inc+emp_status+marriage+edu,data=inRawData)
 
-inContTable <- table(inRawData[,2:dim(inRawData)[2]])
+# number of types: 
+# hh_size(5) * rel(6)  * sex(2) * age(7) * emp_status(5) * marriage(2) * edu(6) * inc(7)
+# 188160
+# this is more than the sample size, hence the problem of zero-cell is severe.
+# we need larger samples.
 
+# convert table to array
+
+target.sex <- c(10126430, 9485938) 
+target.sex.age <- c() 
 
 
  
